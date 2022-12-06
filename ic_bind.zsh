15fc9d0e76a31ef11fe67a1a265cba667fb0e22d
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2022-12-06 09:55:34 +0300

    tiny fixes

--- ic_bind.zsh
+++ ic_bind.zsh
@@ -1,13 +1,19 @@
-typeset "$1"="$2"
+typeset "${1}_dir"="$2"
 
 $1() {
-	(cd "${(P)${funcstack[1]}}" && zsh -i "$1")
+	local name="${funcstack[1]}"
+	local dirname="${name}_dir"
+	local dir="${(P)dirname}"
+	(cd "$dir" && zsh -i "$1")
 }
 
 # Completions
 _$1() {
-	local -a reqs=(${(@f)$(find "${(P)${funcstack[1]:1}}" -type f -name "*.zsh" -not -name "ic.conf.zsh" -printf "%P\n")})
-	_describe epguq reqs
+	local name="${funcstack[1]:1}"
+	local dirname="${name}_dir"
+	local dir="${(P)dirname}"
+	local -a files=(${(@f)$(find "$dir" -type f -name "*.zsh" -not -name "ic.conf.zsh" -printf "%P\n")})
+	_describe "$name" files
 }
 
 compdef "_$1" "$1"
